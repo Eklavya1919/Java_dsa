07940184a04f8e586cf3b7b24385a4f0c3c32381
Eklavya <eklavyashridharshinde@gmail.com>
2026-03-10 22:00:29 +0530

    num of char of last word

---
+++ num_of_lastword.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class num_of_lastword {
+    public static int lengthOfLastWord(String s) {
+        s = s.trim();
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') {
+                break;
+            }
+            length++;
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String input = sc.nextLine();
+        int result = lengthOfLastWord(input);
+        System.out.println("Number of characters in last word: " + result);
+        sc.close();
+    }
+}
